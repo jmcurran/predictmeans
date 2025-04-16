745fa98b402d06ea69e0d6107f58cad0638ed6b1
James Curran <j.curran.auckland.ac.nz>
2025-04-16 14:15:53 +1200

    cleaned up to line 659. Have changed the if at this line to else if because only one of the cases can be true. Will change the next condition as well.

--- R/predictmeans.R
+++ R/predictmeans.R
@@ -534,79 +534,131 @@ predictmeans <- function (model, modelterm, data=NULL, pairwise=FALSE, atvar=NUL
     } # if (is.null(atvar))
   }# end of if(pairwise)
 
-    meanTable <- mt
-    if (is.null(permlist) || all(permlist%in%c("NULL", ""))) {
-      meanTable$LL <- meanTable$pm - qt(1 - slevel/2, df = meanTable$Df) * meanTable$ses
-      meanTable$UL <- meanTable$pm + qt(1 - slevel/2, df = meanTable$Df) * meanTable$ses
-    }else{
-      meanTable$LL <- meanTable$pm - 2 * meanTable$ses
-      meanTable$UL <- meanTable$pm + 2 * meanTable$ses
-      slevel <- 0.05
-      meanTable$Df <- NA
+  meanTable <- mt
+
+  if (is.null(permlist) || all(permlist%in%c("NULL", ""))) {
+    meanTable$LL <- meanTable$pm - qt(1 - slevel/2, df = meanTable$Df) * meanTable$ses
+    meanTable$UL <- meanTable$pm + qt(1 - slevel/2, df = meanTable$Df) * meanTable$ses
+  }else{
+    meanTable$LL <- meanTable$pm - 2 * meanTable$ses
+    meanTable$UL <- meanTable$pm + 2 * meanTable$ses
+    slevel <- 0.05
+    meanTable$Df <- NA
+  }
+
+  rownames(meanTable) <- NULL
+  meanTable <- meanTable[c(vars, "pm", "ses", "Df", "LL", "UL")]
+  meanTable[c("pm", "LL", "UL")] <- round(meanTable[c("pm", "LL", "UL")], ndecimal)
+  meanTable$ses <- round(meanTable$ses, ndecimal+1)
+  colnames(meanTable) <- c(vars, "Mean", "SE", "Df", paste("LL(", (1 - slevel) * 100, "%)", sep = ""),
+                           paste("UL(", (1 - slevel) * 100, "%)", sep = ""))
+
+  if (plot) {
+
+    if (length(vars) > 3){
+      cat("\n", "There is no plot for more than three-way interaction! \n\n")
     }
 
-    rownames(meanTable) <- NULL
-    meanTable <- meanTable[c(vars, "pm", "ses", "Df", "LL", "UL")]
-    meanTable[c("pm", "LL", "UL")] <- round(meanTable[c("pm", "LL", "UL")], ndecimal)
-    meanTable$ses <- round(meanTable$ses, ndecimal+1)
-    colnames(meanTable) <- c(vars, "Mean", "SE", "Df", paste("LL(", (1 - slevel) * 100, "%)", sep = ""),
-                             paste("UL(", (1 - slevel) * 100, "%)", sep = ""))
+    plotmt <- na.omit(mt)
+    yMin <- min(plotmt[, "pm"])
+    yMax <- max(plotmt[, "pm"])
+    offSet <- 0.25 * (yMax - yMin)
 
-    if (plot) {
-      if (length(vars) > 3)
-        cat("\n", "There is no plot for more than three-way interaction! \n\n")
-      plotmt <- na.omit(mt)
-      yMin <- min(plotmt[, "pm"])
-      yMax <- max(plotmt[, "pm"])
-      offSet <- 0.25 * (yMax - yMin)
-
-      if (is.null(atvar) || all(atvar%in%c("NULL", ""))) {
-        if (lsd_bar) LSD_value <- LSD[3] else LSD_value <- SED.out[3]
+    if (is.null(atvar) || all(atvar%in%c("NULL", ""))) {
+      if (lsd_bar){
+        LSD_value <- LSD[3]
       }else{
-        if (lsd_bar)  LSD_value <- mean(attr(LSD,"For the Same Level of Factor")[1, atvar])
-        else LSD_value <- mean(attr(SED.out, "For the Same Level of Factor")[1, atvar])
+        LSD_value <- SED.out[3]
       }
-      if (lsd_bar)  bar_label <- "Aveg.LSD" else bar_label <- "Aveg.SED"
-      up <- yMin + LSD_value
-      lsdBar <- cbind(plotmt[, vars, drop=FALSE], up=up, yMin=yMin)
-      limits <- aes(ymax = (pm + ses)*(pm > 0) + pmin(pm + ses, 0)*(pm <= 0), ymin=(pm - ses)*(pm < 0) + pmax(pm - ses, 0)*(pm >= 0))
+    }else{
+      if (lsd_bar){
+        LSD_value <- mean(attr(LSD,"For the Same Level of Factor")[1, atvar])
+      }else{
+        LSD_value <- mean(attr(SED.out, "For the Same Level of Factor")[1, atvar])
+      }
+    }
 
-      if (length(vars) == 1) {
-        mxlab <- plotxlab
-        if (is.null(plotxlab) || plotxlab%in%c("NULL", "")) mxlab <- paste("\n", vars, sep="")
-        mylab <- plotylab
-        if (is.null(plotylab) || plotylab%in%c("NULL", "")) mylab <- paste(response, "\n", sep="")
-        if (mplot) {
-          if (newwd) dev.new()
-          mtitle <- plottitle
-          if (is.null(plottitle) || plottitle%in%c("NULL", "")) mtitle <- paste("Predicted means for \"", vars, "\" with ", bar_label, " (", slevel * 100, "%) Bar", sep="")
-          p1 <- ggplot(plotmt, aes(eval(parse(text = vars)), pm, group=1))+
+    if (lsd_bar){
+      bar_label <- "Aveg.LSD"
+    }else{
+      bar_label <- "Aveg.SED"
+    }
+
+    up <- yMin + LSD_value
+    lsdBar <- cbind(plotmt[, vars, drop=FALSE], up=up, yMin=yMin)
+    limits <- aes(ymax = (pm + ses)*(pm > 0) + pmin(pm + ses, 0)*(pm <= 0), ymin=(pm - ses)*(pm < 0) + pmax(pm - ses, 0)*(pm >= 0))
+
+    if (length(vars) == 1) {
+      mxlab <- plotxlab
+
+      if (is.null(plotxlab) || plotxlab%in%c("NULL", "")){
+        mxlab <- paste("\n", vars, sep="")
+      }
+
+      mylab <- plotylab
+
+      if (is.null(plotylab) || plotylab%in%c("NULL", "")){
+        mylab <- paste(response, "\n", sep="")
+      }
+
+      if (mplot) {
+        if (newwd){
+          dev.new()
+        }
+
+        mtitle <- plottitle
+
+        if (is.null(plottitle) || plottitle%in%c("NULL", "")){
+          mtitle <- paste("Predicted means for \"", vars, "\" with ", bar_label, " (", slevel * 100, "%) Bar", sep="")
+        }
+
+        p1 <- ggplot(plotmt, aes(eval(parse(text = vars)), pm, group=1))+
             labs(title=paste(mtitle, "\n", sep=""), x=mxlab, y=mylab)+
             lims(x= c(bar_label, levels(plotmt[, vars])), y = c(yMin - offSet, max(yMax + offSet, yMin + LSD_value + offSet))) +
             geom_point(colour="red", size=2)+
           #  geom_line(linewidth=0.5)+
             geom_errorbar(aes(ymax=up, ymin=yMin, x=bar_label), width=0.15, linewidth=0.8, colour="blue") +  #data=lsdBar,
             theme_bw(basesz)
-		  if (lineplot) p1 <- p1 + geom_line(linewidth=0.5)
-          meanPlot <- p1
-          if (prtplt) print(p1)
-        } # end of if mplot
-        if (barplot) {
-          if (newwd) dev.new()
-          mtitle <- plottitle
-          if (is.null(plottitle) || plottitle%in%c("NULL", "")) mtitle <- paste("Predicted means for \"", modelterm, "\" with SE Bars", sep = "")
-          dodge <- position_dodge(width=0.9)
-          bp1 <- ggplot(plotmt, aes(eval(parse(text = vars)), pm))+
+
+  		  if (lineplot){
+  		    p1 <- p1 + geom_line(linewidth=0.5)
+  		  }
+
+        meanPlot <- p1
+
+        if (prtplt){
+          print(p1)
+        }
+      } # end of if mplot
+
+      if (barplot) {
+
+        if (newwd){
+          dev.new()
+        }
+
+        mtitle <- plottitle
+
+        if (is.null(plottitle) || plottitle%in%c("NULL", "")){
+          mtitle <- paste("Predicted means for \"", modelterm, "\" with SE Bars", sep = "")
+        }
+
+        dodge <- position_dodge(width=0.9)
+        bp1 <- ggplot(plotmt, aes(eval(parse(text = vars)), pm))+
             labs(title=paste(mtitle, "\n", sep=""), x=mxlab, y=mylab)+
             ylim(c(min(0, (min(pm) - max(ses)) * 1.2), max(0, (max(pm) + max(ses)) * 1.2))) +
             geom_bar(position=dodge, stat="identity", fill="papayawhip", colour="darkgreen") +
             geom_errorbar(limits, position=dodge, width=0.25, colour="blue")+theme_bw(basesz)+
             theme(panel.border = element_blank(), axis.line = element_line(), panel.grid = element_blank())
-          predictmeansBarPlot <- bp1
-          if (prtplt) print(bp1)
+
+        predictmeansBarPlot <- bp1
+        if (prtplt){
+          print(bp1)
         }
       }
-      if (length(vars) == 2) {
+    }else if (length(vars) == 2) {
+      ##NB: I have changed this to an else statement because there shouldn't be any other way to get here. That is, length(vars) will not
+      ##change from 1 to 2 between the block where length(vars)==1 is true and here, therefore the cases should be else if statements.
         if (is.null(plotord) || all(plotord%in%c("NULL", ""))) {
           plotord <- 1:2
           if (!(is.null(atvar) || all(atvar%in%c("NULL", "")))){
