62bcf176ec7ba2afae79075821fdd40b9c30eb86
jmcurran <james.m.curran@gmail.com>
2025-04-25 13:41:00 +1200

    Cleaned lintr::lint() warnings in Kmatrix.R with usual provisos.
    Cleaned example code in covariatemeans.R

--- R/Kmatrix.R
+++ R/Kmatrix.R
@@ -97,25 +97,23 @@ Kmatrix <- function(model,
 
   Terms <- delete.response(Terms)
 
-  # get the pure formula w/o extra stuff
+  ## get the pure formula w/o extra stuff
   formrhs <- formula(Terms)
-  # if (random_term && inherits(model, "merMod")) formrhs <- formula(model)[-2]
-  # All the variables in the model
+  ## All the variables in the model
   nm <- all.vars(formrhs)
 
   nm <- nm[nm != "pi"]
-  # Figure out if any are coerced to factor or ordered
+  ## Figure out if any are coerced to factor or ordered
   anm <- all.names(formrhs)
   coerced <- anm[1 + grep("factor|as.factor|ordered|as.ordered", anm)]
 
   # Obtain a simplified formula -- needed to recover the data in the model
   form <- as.formula(paste("~", paste(nm, collapse = "+")))
   envir <- attr(Terms, ".Environment")
-  #eval(thecall$data, envir=envir)
 
   if (inherits(model, "mer") ||
-      inherits(model, "merMod") ||
-      inherits(model, "lme") || inherits(model, "gls")) {
+        inherits(model, "merMod") ||
+        inherits(model, "lme") || inherits(model, "gls")) {
     model_data <- get_all_vars(formula(terms(model)), getData(model))
     model_data <- droplevels(na.omit(model_data))
     model_data <- get_all_vars(formrhs, model_data)
@@ -133,7 +131,8 @@ Kmatrix <- function(model,
 
   if (is.null(covariate) || !setequal(covariate, modelterm)) {
     vars <- unlist(strsplit(modelterm, "\\:"))
-    if (any(!is.element(vars, names(model_data)[sapply(model_data, is.factor)]))) {
+    if (any(!is.element(vars,
+                        names(model_data)[sapply(model_data, is.factor)]))) {
       stop(paste(vars, "must be factor(s) or not in the fixed effects!"))
     }
   }
@@ -182,19 +181,16 @@ Kmatrix <- function(model,
   }
 
   if ((!is.null(covariate) &&
-       !unique(covariate %in% c("NULL", ""))) &&
-      all(is.numeric(covariate))) {
+         !unique(covariate %in% c("NULL", ""))) &&
+        all(is.numeric(covariate))) {
     baselevs[covlevname] <- as.list(covariate)
   }
-  if ((!is.null(covariate) &&
-       !unique(covariate %in% c("NULL", ""))) &&
-      is.character(covariate) && all(covariate %in% covlevname)) {
+  if ((!is.null(covariate) && !unique(covariate %in% c("NULL", ""))) &&
+        is.character(covariate) && all(covariate %in% covlevname)) {
+
     if (length(covariate) == 1) {
-      # if (as.is) {
-      # baselevs[[covariate]] <- sort(unique(X[[covariate]]))
-      # }else{
-      if ((!is.null(covariateV) &&
-           !unique(covariateV %in% c("NULL", ""))) && is.vector(covariateV)) {
+      if ((!is.null(covariateV) && !unique(covariateV %in% c("NULL", ""))) &&
+            is.vector(covariateV)) {
         baselevs[[covariate]] <- covariateV
       } else {
         baselevs[[covariate]] <- sort(unique(c(seq(
@@ -203,15 +199,17 @@ Kmatrix <- function(model,
       }
       # }
     } else {
-      if ((!is.null(covariateV) &&
-           !unique(covariateV %in% c("NULL", ""))) && is.list(covariateV)) {
-        for (i in seq_along(covariate))
+      if ((!is.null(covariateV) && !unique(covariateV %in% c("NULL", ""))) &&
+            is.list(covariateV)) {
+        for (i in seq_along(covariate)) {
           baselevs[[covariate[i]]] <- sort(unique(covariateV[[i]]))
+        }
       } else {
-        for (i in covariate)
+        for (i in covariate) {
           baselevs[[i]] <- sort(unique(c(seq(
             min(X[[i]]), max(X[[i]]), length = 30
           ))))
+        }
       }
     }
   }
@@ -221,15 +219,19 @@ Kmatrix <- function(model,
     cat("\n")
   }
 
-  # OK. Now make a grid of the factor levels of interest, along w/ covariate "at" values
+  ## OK. Now make a grid of the factor levels of interest, along w/
+  ## covariate "at" values
   grid <- do.call("expand.grid", baselevs)
 
-  # add any matrices
-  for (nm in names(matdat))
-    grid[[nm]] <- matrix(rep(matdat[[nm]], each = nrow(grid)), nrow = nrow(grid))
+  ## add any matrices
+  for (nm in names(matdat)){
+    grid[[nm]] <- matrix(rep(matdat[[nm]], each = nrow(grid)),
+                         nrow = nrow(grid))
+  }
 
-  # Now make a new dataset with just the factor combs and covariate values we want for prediction
-  # WARNING -- This will overwrite X, so get anything you need from X BEFORE we get here
+  ## Now make a new dataset with just the factor combs and covariate values we
+  ## want for prediction WARNING -- This will overwrite X, so get anything you
+  ## need from X BEFORE we get here
   if (!is.null(ndf)) {
     ndf <- merge(grid, ndf, sort = FALSE)
   }
@@ -237,30 +239,25 @@ Kmatrix <- function(model,
   m <- model.frame(Terms, grid, na.action = na.pass, xlev = xlev)
   X <- model.matrix(Terms, m, contrasts.arg = contrasts)
 
-  # if (!is.null(ndf) && any(ndf$Freq==0)) {
-  # warning("Missing treatments' combination appeared, predicted means maybe misleading!")
-  # X[ndf$Freq==0, ] <- NA
-  # }
-
   max_ord <- max(attr(Terms, "order"))
-  if (!setequal(covariate, modelterm) &&
-      max_ord > 1 &&
-      length(factor_names) > 1  &&
-      any(ndf$Freq == 0) &&
-      !(setequal(unique(ndf$Freq), c(0, 1)))) {
-    #  && any(ndf$Freq==0)
 
-    warning("Missing treatments' combination appeared, predicted means maybe misleading!")
+  if (!setequal(covariate, modelterm) &&
+        max_ord > 1 &&
+        length(factor_names) > 1  &&
+        any(ndf$Freq == 0) &&
+        !(setequal(unique(ndf$Freq), c(0, 1)))) {
+
+    warning(paste0("Missing treatments' combination appeared, predicted means",
+                   "maybe misleading!"))
     X[ndf$Freq == 0, ] <- NA
-    # X <- X[ndf$Freq!=0, ]
   }
 
 
 
-  # All factors (excluding covariates)
+  ## All factors (excluding covariates)
   allFacs <- all.var.names
 
-  ### Array of indexes for rows of X, organized by dimensions
+  ## Array of indexes for rows of X, organized by dimensions
   row.indexes <- array(seq_len(nrow(X)), sapply(baselevs, length))
 
   # convert a string to a formula
@@ -268,14 +265,14 @@ Kmatrix <- function(model,
 
   # These are the variables involved; and the label to use in the results
   facs <- all.vars(form)
-  if ((!is.null(covariate) &&
-       !unique(covariate %in% c("NULL", ""))) &&
-      all(is.character(covariate), all(!covariate %in% facs))) {
+  if ((!is.null(covariate) && !unique(covariate %in% c("NULL", ""))) &&
+        all(is.character(covariate), all(!covariate %in% facs))) {
     facs <- c(facs, covariate)
   }
 
-  if (any(sapply(facs, function(nm)
-    length(grep(nm, allFacs)) == 0))) {
+  if (any(sapply(facs, function(nm) {
+    length(grep(nm, allFacs)) == 0
+  }))) {
     stop(paste(
       "Unknown factor(s) in specification:",
       paste(form, collapse = " ")
@@ -283,8 +280,9 @@ Kmatrix <- function(model,
   }
   # create the grid of factor combinations
   levs <- list()
-  for (f in facs)
+  for (f in facs) {
     levs[[f]] <- baselevs[[f]]
+  }
 
   combs <- do.call("expand.grid", levs)
 
