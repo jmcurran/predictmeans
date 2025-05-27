cf50b26f26403c67ec3c0aa0cfc473cfaceab590
James Curran <j.curran.auckland.ac.nz>
2025-05-27 15:41:47 +1200

    All imports needed in CookD.R added

--- R/CookD.R
+++ R/CookD.R
@@ -1,10 +1,10 @@
 #' Calculates and plots Cook's distances for a Linear (Mixed) Model
-#' 
+#'
 #' This function produces Cook's distance plots for a linear model obtained
 #' from functions \code{aov}, \code{lm}, \code{glm}, \code{gls}, \code{lme}, or
 #' \code{lmer}.
-#' 
-#' 
+#'
+#'
 #' @param model Model object returned by \code{aov}, \code{lm}, \code{glm},
 #' \code{gls}, \code{lme}, and \code{lmer}.
 #' @param group Name (in "quotes") for indicating how observations are deleted
@@ -19,14 +19,19 @@
 #' window. The default value is FALSE.
 #' @author Dongwen Luo, Siva Ganesh and John Koolaard
 #' @examples
-#' 
+#'
 #' library(predictmeans)
 #' Oats$nitro <- factor(Oats$nitro)
 #' fm <- lme(yield ~ nitro*Variety, random=~1|Block/Variety, data=Oats)
 #' # library(lme4)
 #' # fm <- lmer(yield ~ nitro*Variety+(1|Block/Variety), data=Oats)
 #' CookD(fm)
-#' 
+#'
+#' @importFrom stats update
+#' @importFrom graphics points text
+#' @importFrom grDevices dev.new
+#'
+#' @export
 CookD <- function (model, group=NULL, plot=TRUE, idn=3, newwd=FALSE) {
 
   stopifnot("CookD doesn't support this model!"={
