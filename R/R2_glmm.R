585cb1c7cfb45c3272206bd7ce93b398f3772ad8
James Curran <j.curran.auckland.ac.nz>
2025-05-28 13:52:30 +1200

    altered R2_glmm to make sure function exported

--- R/R2_glmm.R
+++ R/R2_glmm.R
@@ -1,10 +1,10 @@
 #' An adjusted coefficient of determination (R2) for generalized linear mixed
 #' models
-#' 
+#'
 #' This function produces adjusted R2 for generalized linear mixed models which
 #' was crafted following the guidance provided by Professor Hans-Peter Piepho.
-#' 
-#' 
+#'
+#'
 #' @param model An object returned by \code{lmer}, \code{glmer} or
 #' \code{glmmTMB}.
 #' @param over_disp A logical scalar to indicate whether \code{model} with
@@ -15,15 +15,16 @@
 #' generalized linear mixed models in one go. Biom J. 2023 Oct;65(7):e2200290.
 #' doi: 10.1002/bimj.202200290. Epub 2023 May 1. PMID: 37127864.
 #' @examples
-#' 
+#'
 #'   library(predictmeans)
 #'   Oats$nitro <- factor(Oats$nitro)
 #'   (fm <- lmer(yield ~ nitro*Variety+(1|Block/Variety), data=Oats))
 #'   R2_glmm(fm)
 #'   (gm <- glmer(cbind(incidence, size - incidence) ~ period + (1 | herd),
 #'               data = cbpp, family = binomial))
-#'   R2_glmm(gm)		  
-#' 
+#'   R2_glmm(gm)
+#'
+#' @export
 R2_glmm <- function(model, over_disp=FALSE) {
   stopifnot(
     any(
@@ -32,14 +33,14 @@ R2_glmm <- function(model, over_disp=FALSE) {
       inherits(model, "glmmTMB")
     )
   )
-  
+
   tr <- function (m) {
     if ((dim(m)[1] != dim(m)[2])) {
       stop("m must be a square matrix")
     }
     return(sum(diag(m), na.rm = TRUE))
-  }  
-  
+  }
+
   if (inherits(model, "glmmTMB")) {
     tmb_call <- model$call
     tmb_call$doFit <- FALSE
@@ -52,7 +53,7 @@ R2_glmm <- function(model, over_disp=FALSE) {
     weights <- tmbf$data.tmb$weights
     cond_reTrms <- reTrms_tmb(model)
     Lambda <- t(cond_reTrms$Lambdat)
-    
+
     beta <- getME(model, "beta")
     beta_L <- grep("betadisp|betazi", names(beta))
     if (length(beta_L > 0)) {
@@ -86,17 +87,17 @@ R2_glmm <- function(model, over_disp=FALSE) {
     }
     covB <- vcov(model)
   }
-  
-  X <- getME(model, "X") 
-  Z <- getME(model, "Z") 
+
+  X <- getME(model, "X")
+  Z <- getME(model, "Z")
   XZ <- cbind(X, Z)
-  
+
   if (family_n != "gaussian") {
     if (family_n =="binomial" && all(yobs%in%c(0,1))) {
       prior_weight <- switch(link_n,
                              "logit" = 3/(pi^2),
                              "probit" = 1,
-                             "cloglog" = 6/(pi^2))	
+                             "cloglog" = 6/(pi^2))
     } else {
       if (inherits(model, "glmmTMB")) {
         weight_fun <- function(eta, mod) {
@@ -104,7 +105,7 @@ R2_glmm <- function(model, over_disp=FALSE) {
           family_fun$mu.eta(eta)^2/family_fun$variance(family_fun$linkinv(eta))
         }
         y_hat <- as.vector(XZ %*% c(beta, b))
-        prior_weight <- as.vector(weight_fun(y_hat, model))*tmbf$data.tmb$weights 
+        prior_weight <- as.vector(weight_fun(y_hat, model))*tmbf$data.tmb$weights
         if (length(tmbf$data.tmb$size) > 0) {
           prior_weight <- prior_weight*tmbf$data.tmb$size
         }
@@ -115,13 +116,13 @@ R2_glmm <- function(model, over_disp=FALSE) {
   } else {
     prior_weight <- weights
   }
-  
-  n <- nrow(X) 
+
+  n <- nrow(X)
   Zt <- t(Z)
-  s <- sigma(model)    
+  s <- sigma(model)
   G <- Matrix::tcrossprod(Lambda)*(s^2)
   V <- Z %*% G %*% Zt+diag(n)*(s^2/prior_weight)
-  P <- diag(n) - matrix(1/n, n, n) 
+  P <- diag(n) - matrix(1/n, n, n)
   theta_V <- tr(V %*% P)/(n-1)
   theta_Xbeta <- t(beta) %*% t(X) %*% P %*% X %*% (beta/(n-1)) - tr(t(X) %*% P %*% X %*% covB)/(n-1)
   if (over_disp) {
@@ -129,16 +130,16 @@ R2_glmm <- function(model, over_disp=FALSE) {
   } else {
     theta_Zu <- tr(P %*% Z %*% G %*% Zt)/(n-1)
   }
-  
+
   Omega_full <- theta_Xbeta+theta_V
   Omega_beta <- theta_Xbeta/Omega_full
   Omega_u <- theta_Zu/Omega_full
   Omega_beta_u <- Omega_beta+Omega_u
-  
+
   Omega_beta_percent=Omega_beta*100
   Omega_u_percent=Omega_u*100
   Omega_beta_u_percent=Omega_beta_u*100
-  
+
   if (!over_disp) {
     if (inherits(model, "glmmTMB")) {
       u_ind <- c(ini=0, sapply(cond_reTrms$Ztlist, function(x) x@Dim[1]))
@@ -150,10 +151,10 @@ R2_glmm <- function(model, over_disp=FALSE) {
       u_indn <- sub("\\.\\(Intercept\\)$", "",names(u_ind))
       names(u_ind) <- u_indn
     }
-    
+
     Omega_u_list <- vector("list", length(u_indn)-1)
     names(Omega_u_list) <- u_indn[-1]
-    
+
     for (i in 1:length(Omega_u_list)) {
       contInds <- (u_ind[i] + 1):(u_ind[i + 1])
       theta_Zu_i <- tr(P %*% Z[, contInds] %*% G[contInds, contInds] %*% Zt[contInds,])/(n-1)
@@ -162,7 +163,7 @@ R2_glmm <- function(model, over_disp=FALSE) {
   } else {
     Omega_u_list <- NULL
   }
-  
+
   cod_v <- c(Total=Omega_beta_u_percent, Fixed=Omega_beta_percent, Random=Omega_u_percent,  unlist(Omega_u_list)*100)
   R2 <- paste(round(cod_v, 2), "%", sep="")
   names(R2) <- names(cod_v)
