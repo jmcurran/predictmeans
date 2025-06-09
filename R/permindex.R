f7f00bf8a3b7dc65d193a8c9a551e75e49c86406
James Curran <j.curran.auckland.ac.nz>
2025-06-09 15:44:23 +1200

    Cleaned all lint warnings from permindex.R

--- R/permindex.R
+++ R/permindex.R
@@ -13,23 +13,23 @@
 #' @author Dongwen Luo, Siva Ganesh and John Koolaard
 #' @examples
 #'
-#'   library(predictmeans)
-#'   block <- rep(1:3, each=12)
-#'   group <- rep(rep(1:3, each=4), 3)
-#'   data <- data.frame(block, group)
-#'   cbind(data, permindex(data, block="block", group="group", nsim=5))
-#'                         # Permute group as a whole within each block first,
-#'                         # then permute obs within each group.
-#'   cbind(data, permindex(data, block="block",  nsim=5))
-#'                         # Permute obs within each block only.
-#'   cbind(data, permindex(data, group="group", nsim=5))
-#'                         # Permute groups as a whole block first,
-#'                         # then permute obs within each group.
-#'   cbind(data, permindex(data, nsim=5))  # Free permutation.
+#' library(predictmeans)
+#' block <- rep(1:3, each = 12)
+#' group <- rep(rep(1:3, each = 4), 3)
+#' data <- data.frame(block, group)
+#' cbind(data, permindex(data, block = "block", group = "group", nsim = 5))
+#' # Permute group as a whole within each block first,
+#' # then permute obs within each group.
+#' cbind(data, permindex(data, block = "block", nsim = 5))
+#' # Permute obs within each block only.
+#' cbind(data, permindex(data, group = "group", nsim = 5))
+#' # Permute groups as a whole block first,
+#' # then permute obs within each group.
+#' cbind(data, permindex(data, nsim = 5)) # Free permutation.
 #' @export
-permindex <- function(data, block=NULL, group=NULL, nsim=4999, seed) {
+permindex <- function(data, block = NULL, group = NULL, nsim = 4999, seed) {
   if (any(!is.null(block), !is.null(group))) {
-    data <- data[do.call(order, data[, c(block, group), drop=FALSE]),]
+    data <- data[do.call(order, data[, c(block, group), drop = FALSE]), ]
   }
   rownames(data) <- NULL
   n <- nrow(data)
@@ -39,35 +39,36 @@ permindex <- function(data, block=NULL, group=NULL, nsim=4999, seed) {
     }
     permIndex <- replicate(nsim, sample.int(n))
   } else if (all(!is.null(block), is.null(group))) {
-    Bsplit <- split(seq_len(n), as.character(data[, block]))    # split 1:n seq into nB blocks
+    Bsplit <- split(seq_len(n), as.character(data[, block])) # split 1:n seq into nB blocks
     if (!missing(seed)) {
-      set.seed(seed+1)
+      set.seed(seed + 1)
     }
-    permIndex <- replicate(nsim, unlist(lapply(Bsplit, function (x) x[sample.int(length(x))]), recursive = FALSE, use.names = FALSE))
-  } else if (all(is.null(block), !is.null(group))) {   # data=data; group="group"; nsim=5
+    permIndex <- replicate(nsim, unlist(lapply(Bsplit, function(x) x[sample.int(length(x))]),
+                                        recursive = FALSE, use.names = FALSE))
+  } else if (all(is.null(block), !is.null(group))) { # data=data; group="group"; nsim=5
     if (!missing(seed)) {
-      set.seed(seed+2)
+      set.seed(seed + 2)
     }
     permIndex <- replicate(nsim, {
-      Gsplit <- split(seq_len(n), as.character(data[, group]))   # split 1:n seq into separate groups
-      nGsplit <- lapply(Gsplit, function (x) x[sample.int(length(x))])   # within each group permute the whole seq
+      Gsplit <- split(seq_len(n), as.character(data[, group])) # split 1:n seq into separate groups
+      nGsplit <- lapply(Gsplit, function(x) x[sample.int(length(x))]) # within each group permute the whole seq
       unlist(nGsplit[sample.int(length(nGsplit))], recursive = FALSE, use.names = FALSE)
-    })          # permute the order of the whole groups and repeat process nsim times
+    }) # permute the order of the whole groups and repeat process nsim times
   } else {
     ind <- seq_len(n)
     names(ind) <- as.character(data[, group])
-    Bsplit <- split(ind, as.character(data[, block]))    # split 1:n seq into blocks
+    Bsplit <- split(ind, as.character(data[, block])) # split 1:n seq into blocks
     if (!missing(seed)) {
-      set.seed(seed+3)
+      set.seed(seed + 3)
     }
-    permIndex <- replicate(nsim, unlist(lapply(Bsplit, function (x) {    # within each block
-      Gsplit <- split(x, names(x))  # split each block seq into groups
-      nGsplit <- lapply(Gsplit, function (x) x[sample.int(length(x))])   # within each group permute the whole seq
-      unlist(nGsplit[sample.int(length(nGsplit))], recursive = FALSE, use.names = FALSE)          # permute the order of groups
+    permIndex <- replicate(nsim, unlist(lapply(Bsplit, function(x) { # within each block
+      Gsplit <- split(x, names(x)) # split each block seq into groups
+      nGsplit <- lapply(Gsplit, function(x) x[sample.int(length(x))]) # within each group permute the whole seq
+      unlist(nGsplit[sample.int(length(nGsplit))], recursive = FALSE, use.names = FALSE) # permute the order of groups
     }), recursive = FALSE, use.names = FALSE))
   }
 
-  colnIndex <- apply(permIndex, 2, function(x) paste(x, collapse=""))
+  colnIndex <- apply(permIndex, 2, function(x) paste(x, collapse = ""))
   colnames(permIndex) <- colnIndex
   permIndex <- permIndex[, unique(colnIndex)]
   colnames(permIndex) <- NULL
