188abe268a93188744fbbf35343a7d17c734258b
James Curran <j.curran.auckland.ac.nz>
2025-06-09 15:45:58 +1200

    Cleaned all lint warnings from permlmer.R

--- R/permlmer.R
+++ R/permlmer.R
@@ -31,16 +31,15 @@
 #'
 #' # Test fixed effects
 #' Oats$nitro <- factor(Oats$nitro)
-#' fm0 <- lmer(yield ~ nitro+Variety+(1|Block/Variety), data=Oats)
-#' fm <- lmer(yield ~ nitro*Variety+(1|Block/Variety), data=Oats)
+#' fm0 <- lmer(yield ~ nitro + Variety + (1 | Block / Variety), data = Oats)
+#' fm <- lmer(yield ~ nitro * Variety + (1 | Block / Variety), data = Oats)
 #' permlmer(fm0, fm)
 #' }
 #'
 #' @importFrom parallel clusterEvalQ
 #' @importFrom stats density logLik
 #' @export
-permlmer <- function(lmer0, lmer1, nperm = 999, ncore=3L, plot=FALSE, seed){
-
+permlmer <- function(lmer0, lmer1, nperm = 999, ncore = 3L, plot = FALSE, seed) {
   if (any(!inherits(lmer0, "lmerMod"), !inherits(lmer1, "lmerMod"))) {
     stop("The model must be a lmer object!")
   }
@@ -48,8 +47,9 @@ permlmer <- function(lmer0, lmer1, nperm = 999, ncore=3L, plot=FALSE, seed){
     stop("Please check the response in your model!")
   }
 
-  c_deparse <- function (...)
+  c_deparse <- function(...) {
     paste(deparse(..., width.cutoff = 500), collapse = "")
+  }
   lmernames <- vapply(as.list(sys.call()[-1L]), c_deparse, "")
 
   theta0 <- getME(lmer0, "theta")
@@ -61,22 +61,23 @@ permlmer <- function(lmer0, lmer1, nperm = 999, ncore=3L, plot=FALSE, seed){
   fixef0name <- names(fixef0)
   fixef1name <- names(fixef1)
 
-  term0name <- attr(terms(lmer0),"term.labels")
-  term1name <- attr(terms(lmer1),"term.labels")
+  term0name <- attr(terms(lmer0), "term.labels")
+  term1name <- attr(terms(lmer1), "term.labels")
   term0in1 <- rep(FALSE, length(term0name))
   names(term0in1) <- term0name
   for (i in term0name) {
-    for (j in term1name){
-      if (length(setdiff(unlist(strsplit(i, "\\:")), unlist(strsplit(j, "\\:"))))==0) {
+    for (j in term1name) {
+      if (length(setdiff(unlist(strsplit(i, "\\:")), unlist(strsplit(j, "\\:")))) == 0) {
         term0in1[i] <- TRUE
         break
       }
     }
   }
 
-  # if(!setequal(intersect(theta0name, theta1name), theta0name) || !setequal(intersect(fixef0name, fixef1name), fixef0name)) stop(paste("The model", lmernames[1], "must be nested within the model", lmernames[2]))
+  # if(!setequal(intersect(theta0name, theta1name), theta0name) || !setequal(intersect(fixef0name, fixef1name),
+  # fixef0name)) stop(paste("The model", lmernames[1], "must be nested within the model", lmernames[2]))
 
-  if(!setequal(intersect(theta0name, theta1name), theta0name) || !all(term0in1)) {
+  if (!setequal(intersect(theta0name, theta1name), theta0name) || !all(term0in1)) {
     stop(paste("The model", lmernames[1], "must be nested within the model", lmernames[2]))
   }
 
@@ -88,13 +89,13 @@ permlmer <- function(lmer0, lmer1, nperm = 999, ncore=3L, plot=FALSE, seed){
   Lambda1n <- getME(lmer1, "Lambda")
   Lambda1n@x <- thetan[getME(lmer1, "Lind")]
   Lambda1nc <- Matrix::tcrossprod(Lambda1n)
-  V1n <- getME(lmer1, "Z")%*%Lambda1nc%*%getME(lmer1, "Zt")+diag(dim(getME(lmer1, "Z"))[1])
+  V1n <- getME(lmer1, "Z") %*% Lambda1nc %*% getME(lmer1, "Zt") + diag(dim(getME(lmer1, "Z"))[1])
   Ut <- t(chol(V1n))
   wt <- solve(Ut)
   xbeta <- as.vector(getME(lmer0, "X") %*% fixef0)
   errors <- getME(lmer0, "y") - xbeta
 
-  #Weighting the residuals.
+  # Weighting the residuals.
   wterrors <- wt %*% errors
 
   # permute weighted resid, then unweighted it for 999 times
@@ -105,26 +106,25 @@ permlmer <- function(lmer0, lmer1, nperm = 999, ncore=3L, plot=FALSE, seed){
   # }
   # permy <- as.data.frame(xbeta+permResid)
   if (ref) {
-    if(!missing(seed)) {
+    if (!missing(seed)) {
       set.seed(seed)
     }
-    permy <- as.data.frame(xbeta+replicate(nperm, as.vector(Ut %*% sample(wterrors))))
+    permy <- as.data.frame(xbeta + replicate(nperm, as.vector(Ut %*% sample(wterrors))))
   } else {
     if (!missing(seed)) {
       set.seed(seed)
     }
     permy <- replicate(nperm, {
-      rowindex <- sample(1:length(errors))
-      as.data.frame(xbeta[rowindex]+as.vector(Ut %*% wterrors[rowindex]))
+      rowindex <- sample(seq_along(errors))
+      as.data.frame(xbeta[rowindex] + as.vector(Ut %*% wterrors[rowindex]))
     })
   }
 
   # Calculating the likelihood ratio test statistic for each permutation.
-  lrtest1 <- 2*(logLik(lmer1, REML=ref)-logLik(lmer0, REML=ref))
+  lrtest1 <- 2 * (logLik(lmer1, REML = ref) - logLik(lmer0, REML = ref))
   lrtest1 <- ifelse(lrtest1 < 0, 0, lrtest1)
 
-  if (.Platform$OS.type=="windows") {
-
+  if (.Platform$OS.type == "windows") {
     ## DONGWEN: I have added this function because the code was failing under
     ## devtools::check(). There's a long explanation, but I am trying something
     ## suggested by ChatGPT
@@ -138,35 +138,37 @@ permlmer <- function(lmer0, lmer1, nperm = 999, ncore=3L, plot=FALSE, seed){
     }
 
     ## this tries to respect the request from the function call to permlmer
-    ncore = get_safe_cores(ncore)
+    ncore <- get_safe_cores(ncore)
 
 
     cl <- makeCluster(ncore)
     clusterEvalQ(cl, library(lme4))
     clusterExport(cl, c("lmer0", "lmer1", "ref"), envir = environment())
     lrtest2 <- parLapplyLB(cl, permy, function(x) {
-      LRT <- try(2*(logLik(refit(lmer1, x), REML=ref) - logLik(refit(lmer0, x), REML=ref)), TRUE)
+      LRT <- try(2 * (logLik(refit(lmer1, x), REML = ref) - logLik(refit(lmer0, x), REML = ref)), TRUE)
       LRT <- ifelse(is.numeric(LRT), LRT, NA)
     })
     stopCluster(cl)
   } else {
     lrtest2 <- mclapply(permy, function(x) {
-      LRT <- try(2*(logLik(suppressMessages(refit(lmer1, x)), REML=ref) - logLik(suppressMessages(refit(lmer0, x)), REML=ref)), TRUE)
+      LRT <- try(2 * (logLik(suppressMessages(refit(lmer1, x)), REML = ref) -
+                        logLik(suppressMessages(refit(lmer0, x)), REML = ref)), TRUE)
       LRT <- ifelse(is.numeric(LRT), LRT, NA)
-    }, mc.cores=ncore)
+    }, mc.cores = ncore)
   }
 
-  #Calculating the p-values.
+  # Calculating the p-values.
   lrtest <- na.omit(unlist(lrtest2))
   lrtest <- ifelse(lrtest < 0, 0, lrtest)
-  perm_p <- (sum(lrtest >= lrtest1) +1)/(length(lrtest) + 1)
-  aod <- anova(lmer0, lmer1, refit=!ref)
-  aod$'Perm-p' <- c(NA, perm_p)
+  perm_p <- (sum(lrtest >= lrtest1) + 1) / (length(lrtest) + 1)
+  aod <- anova(lmer0, lmer1, refit = !ref)
+  aod$"Perm-p" <- c(NA, perm_p)
 
   if (plot) {
     # dev.new()
-    plot (density(c(lrtest1, lrtest), kernel = "epanechnikov"), col="blue", lwd=2, xlab = "", main = "LR Test's density kernels")
-    abline(v=lrtest1, col="red")
+    plot(density(c(lrtest1, lrtest), kernel = "epanechnikov"),
+         col = "blue", lwd = 2, xlab = "", main = "LR Test's density kernels")
+    abline(v = lrtest1, col = "red")
   }
   return(aod)
 }
